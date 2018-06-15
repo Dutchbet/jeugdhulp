a6a1cc8d5b20d949b4e8f3f9199b093693ee4c7e
ryan koorn <ryan.spottingames@gmail.com>
2018-06-15 12:26:29 +0200

    V.01.04
    - Antwoord aanpassen input text naar textarea
    - Styling van edit pagina

--- resources/views/reflecties/edit.blade.php
+++ resources/views/reflecties/edit.blade.php
@@ -23,14 +23,15 @@
         <div class="row">
           <div class="col-md-4"></div>
           <div class="form-group col-md-4">
-            <label for="name">Antwoord aanpassen</label>
-            <input type="textarea" class="antwoorden-form" name="antwoord" value="{{$reflecties->antwoord}}">
+            <div class="title-edit">Antwoord aanpassen</div>
+            <textarea name="antwoord" class="antwoorden-form" rows="25" cols="30" value="{{$reflecties->antwoord}}"></textarea>
+<!--            <input type="textarea" class="antwoorden-form" name="antwoord" value="{{$reflecties->antwoord}}"> -->
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-4"></div>
-          <div class="form-group col-md-4" style="margin-top:60px">
+          <div class="form-group col-md-4"">
             <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
           </div>
         </div>
